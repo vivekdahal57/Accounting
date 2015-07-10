e6dda69af9672babd94600f96cdfbc530cb871bf
Vinaya Dahal <vinayadahal@gmail.com>
2015-07-10 23:33:18 +0545

    login page complete

--- mockup/accountMgnt/login.php
+++ mockup/accountMgnt/login.php
@@ -11,22 +11,38 @@
         <script type="text/javascript" src="js/scripts.js"></script>
     </head>
     <body>
+        <nav class="navbar navbar-default navbar-fixed-top" >
+            <div class="container-fluid">
+                <div class="navbar-header" style="float:left">
+                    <div class="logo">
+                        <img src="images/logo.jpg" width="150" height="55">
+                    </div>
+                    <!--<a class="navbar-brand" href="#">Brand</a>-->
+                </div>
+            </div>
+        </nav>
         <div class="panel panel-primary panel_login">
-            <div class="panel-heading login_heading">
+            <div class="login_heading">
                 <h4 class="panel-title">Login - Accounting</h4>
             </div>
             <div class="panel-body input_wrap">
-                <form action="<?php echo baseUrl; ?>apanel/loginCheck" method="POST">
+                <form action="apanel/loginCheck" method="POST">
                     <div class="inner-addon left-addon">
-                        <input type="text" class="form-control form_override" name="username" placeholder="Username"/>
+                        <input type="text" class="form-control" name="username" placeholder="Username"/>
                         <i class="glyphicon glyphicon-user"></i>
                     </div>
                     <div class="inner-addon left-addon">
-                        <input type="password" class="form-control form_override" name="password" placeholder="Password"/>
+                        <input type="password" class="form-control" name="password" placeholder="Password"/>
                         <i class="glyphicon glyphicon-lock"></i>
                     </div>
-                    <div style="margin-top:15px;">
+                    <div style="margin-top:15px;float: right;">
                         <a href="javascript:void(0)">Forgot Password?</a>
+                    </div>
+                    <div class="checkbox" style="margin-top: 15px; ">
+                        <input type="checkbox"  id="remember" style="margin: -8px 5px 0px 0px; display: table-cell;border:none;outline: none;">&nbsp;
+                        <label for="remember">
+                            Remember me
+                        </label>
                     </div>
                     <button type="submit" class="btn btn-default btn_login_override">Login</button>
                 </form>
